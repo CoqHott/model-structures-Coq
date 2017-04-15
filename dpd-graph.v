62398c3b0c9d74a398cb98125da9b2603a57eb08
SimonBoulier <simon.boulier@ens-rennes.fr>
2017-04-15 15:14:53 +0200

    dpd-graph

--- dpd-graph.v
+++ dpd-graph.v
@@ -1,11 +1,10 @@
-Require HTS.Model_structure.
+Require MLTT2.Model_structure.
 
-(* Add ML Path "/home/rascar/.opam/coq/lib/coq/user-contrib/dpdgraph". *)
-Add ML Path "/home/rascar/Dépôts/coq-dpdgraph-bertot".
-Print ML Path.
-
+(* Add ML Path "~/.opam/coq/lib/coq/user-contrib/dpdgraph". *)
+(* Print ML Path. *)
 
 Declare ML Module "dpdgraph".
+
 
 Set DependGraph File "type_model_structure.dpd".
 Print DependGraph Model_structure.type_model_structure.
@@ -14,4 +13,4 @@ Set DependGraph File "Model_structure.dpd".
 Print FileDependGraph Model_structure.
 
 Set DependGraph File "All.dpd".
-Print FileDependGraph ModelStructure.Overture ModelStructure.HTS.Overture HTS.Overture Category Strict_eq Path_eq Equivalences Cylinder Model_structure.
+Print FileDependGraph ModelStructure.Overture ModelStructure.MLTT2.Overture MLTT2.Overture Category Strict_eq Path_eq Equivalences Cylinder Model_structure.
